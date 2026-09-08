7b64ff032e0ed647f5c411349348ae4a2c5775b3
Matt Coleman <matt3188@users.noreply.github.com>
2026-09-08 22:21:17 +0100

    Configure Vite for repository Pages path

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({ plugins: [vue()], test: { environment: 'jsdom' } })
+export default defineConfig({
+  base: '/metronome/',
+  plugins: [vue()],
+  test: { environment: 'jsdom' },
+})
