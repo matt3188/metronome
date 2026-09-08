ffecfd637c4373b5892333f68a8f98873c48c435
Matt Coleman <matt3188@users.noreply.github.com>
2026-09-08 22:21:22 +0100

    Use Vite base URL for Vue Router

--- src/router.ts
+++ src/router.ts
@@ -3,7 +3,7 @@ import HomeView from './views/HomeView.vue'
 import CustomView from './views/CustomView.vue'
 
 export default createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', name: 'home', component: HomeView },
     { path: '/custom', name: 'custom', component: CustomView },
